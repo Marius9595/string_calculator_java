eb01f13464c79ce4ad92488582aaf6bc88932934
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-07-25 21:13:00 +0100

    🔴 StringCalculatorShould add_numbers_separated_by_an_default_delimiter

--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -7,8 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 /*
 "" -> 0 OK
-"1" -> 1
-"1,2" -> 3
+"1" -> 1 OK
 "1,2,3,4" -> 10
 “1\n2,3” ->  6
 “1,\n” -> ERROR
@@ -37,4 +36,9 @@ public class StringCalculatorShould {
     public void not_add_an_expression_with_just_a_number (){
         assertThat(stringCalculator.add("1")).isEqualTo(1);
     }
+    @Test
+    public void add_numbers_separated_by_an_default_delimiter (){
+        assertThat(stringCalculator.add("1,2,3,4")).isEqualTo(10);
+    }
+
 }
